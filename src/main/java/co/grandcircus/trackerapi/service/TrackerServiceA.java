df91d84dd56edf26f9016e635bc3f695ca2fdaaf
NIPHAM53 <nipham53@gmail.com>
2022-08-17 21:52:29 -0400

    Second Lab Day

--- src/main/java/co/grandcircus/trackerapi/service/TrackerServiceA.java
+++ src/main/java/co/grandcircus/trackerapi/service/TrackerServiceA.java
@@ -1,5 +1,7 @@
 package co.grandcircus.trackerapi.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,12 +11,32 @@ import co.grandcircus.trackerapi.model.TrackingList;
 
 @Service
 public class TrackerServiceA implements TrackerService{
-	TrackingList trackingList = new TrackingList();
+
+	private List<CountPair> counts;
+
+	public List<CountPair> getCounts() {
+		return counts;
+	}
+
+	public void setCounts(List<CountPair> counts) {
+		this.counts = counts;
+	}
+
+	public TrackerServiceA() {
+		counts = new ArrayList<CountPair>();
+	}
 
 	@Override
 	public void add(String token) {
-		CountPair countPair = new CountPair(token, 0);
-		trackingList.addToList(countPair);
+
+		for (CountPair count : counts) {
+			if (count.getToken().equals(token)) {
+				count.setCount(count.getCount() + 1);
+				return;
+			}
+		}
+		CountPair newCount = new CountPair(token, 1);
+		counts.add(newCount);
 		
 	}
 
@@ -23,57 +45,127 @@ public class TrackerServiceA implements TrackerService{
 		/**
 		 * Clear out all records. Reset back to no tokens.
 		 */
-	trackingList.deleteAll();
-		
+		counts = new ArrayList<CountPair>();
 		
 	}
 
 	@Override
 	public int getTotalCount() {
-		List<CountPair >countList = trackingList.getCountPairList();
-		int counter = 0;
-		for (CountPair cp : countList) {
-			counter++;
-		}
 
-		return counter;
+		int total = 0;
+		for (CountPair count : counts) {
+			total += count.getCount();
+		}
+		return total;
 		
 	}
 
 	@Override
 	public boolean getTokenExists(String token) {
-		// TODO Auto-generated method stub
+		for (CountPair count : counts) {
+			if(count.getToken().equals(token)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	@Override
 	public int getTokenCount(String token) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (CountPair count : counts) {
+			if (count.getToken().equals(token)) {
+				return count.getCount();
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public String getLatest() {
-		// TODO Auto-generated method stub
-		return null;
+		/**
+		 * Return the most recent token tracked
+		 */
+		
+		String latest = "";
+		if(counts.size()==0) {
+			return latest;
+		}
+		for (int i = counts.size() - 1 ; i < counts.size() ; i++) {
+			 latest = counts.get(counts.size() - 1).getToken();
+		}
+			
+
+		return latest;
 	}
 
 	@Override
 	public CountPair getTop() {
-		// TODO Auto-generated method stub
-		return null;
+		/**
+		 * Return the token that has been tracked the most times AND how many times
+		 * it has been tracked. If there are no tokens return a CountPair("", 0).
+		 * 
+		 * @return a CountPair including the token and count
+		 */
+		CountPair highestCount = new CountPair("",0);
+		for (CountPair count : counts) {
+		if (count.getCount() > highestCount.getCount()) {
+			highestCount = count;
+		}
+		}
+		return highestCount;
+		
 	}
 
 	@Override
 	public List<String> getLatest5() {
-		// TODO Auto-generated method stub
-		return null;
+		/**
+		 * Return five the most recent token tracked in order with the most recent
+		 * first. The list may contain duplicates if the same token was registered
+		 * multiple times. If there are less than five total hits, return all the
+		 * available tokens.
+		 */
+		List<String> latest = new ArrayList<>();
+		
+		if(counts.size()==0) {
+			return latest;
+		}
+		if(counts.size() <5) {
+			for (CountPair count: counts) {
+				latest.add(count.getToken());
+			}
+			return latest;
+		}
+		for (int i = counts.size() - 5 ; i < counts.size() ; i++) {
+			 latest.add(counts.get(i).getToken());
+			 
+		}
+			
+
+		return latest;
 	}
 
 	@Override
 	public List<CountPair> getTop5() {
-		// TODO Auto-generated method stub
-		return null;
+		/**
+		 * Return the five tokens that have been tracked the most times AND how many
+		 * times they have been tracked. If there are less than five unique tokens,
+		 * return CountPairs the available tokens.
+		 * 
+		 * @return a List of CountPair ordered with the highest count first.
+		 */
+		
+		
+		
+		List<CountPair> topFive = new ArrayList<>();
+		if (counts.size()==0) {
+			return topFive;
+		}
+		if (counts.size() > 4) {
+			
+			
+		}
+		return topFive;
 	}
 
 }
--- src/test/java/co/grandcircus/trackerapi/TrackerServiceTests.java
+++ src/test/java/co/grandcircus/trackerapi/TrackerServiceTests.java
@@ -2,6 +2,9 @@ package co.grandcircus.trackerapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -103,5 +106,73 @@ public class TrackerServiceTests {
 	void testTopNoResult() {
 		assertEquals(new CountPair("", 0), service.getTop());
 	}
+	@Test
+	void testReset () {
+		service.add("Alpha");
+		service.reset();
+		assertEquals(0, service.getTotalCount());
+	}
+	@Test
+	void testGetTokenExist() {
+		service.add("Alpha");
+		assertEquals(true, service.getTokenExists("Alpha"));
+	}
+	@Test
+	void testGetTokenExistFalse() {
+		service.add("Alpha");
+		assertEquals(false, service.getTokenExists("Beta"));
 
+}
+	@Test
+	void testGetTokenCount() {
+		service.add("Alpha");
+		service.add("Alpha");
+		service.add("Alpha");
+		service.add("Gamma");
+		assertEquals(3, service.getTokenCount("Alpha"));
+		
+	}
+	@Test
+	void testLatest5() {
+		service.add("Alpha");
+		service.add("Beta");
+		service.add("Gamma");
+		service.add("Delta");
+		service.add("Epsilon");
+		service.add("Alpha");
+		service.add("Beta");
+		service.add("Gamma");
+		service.add("Delta");
+		service.add("Alpha");
+		service.add("1");
+		service.add("2");
+		service.add("3");
+		service.add("4");
+		service.add("5");
+		List<String> latest5 = new ArrayList<>();
+		latest5.add("1");
+		latest5.add("2");
+		latest5.add("3");
+		latest5.add("4");
+		latest5.add("5");
+		assertEquals(latest5, service.getLatest5());
+}
+	@Test
+	void testLatest5If0() {
+		List<String> latest5 = new ArrayList<>();
+		assertEquals(latest5, service.getLatest5());
+}
+	@Test
+	void testLatest5IfLessThan5() {
+		service.add("2");
+		service.add("3");
+		service.add("4");
+		service.add("5");
+		List<String> latest5 = new ArrayList<>();
+		latest5.add("2");
+		latest5.add("3");
+		latest5.add("4");
+		latest5.add("5");
+		assertEquals(latest5, service.getLatest5());
+}
 }
